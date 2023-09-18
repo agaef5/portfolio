1f4de46845a413007d643681423f6926115203d7
Aga Furgalska <95569676+agaef5@users.noreply.github.com>
2023-09-18 16:31:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function(){
+    const loading_screen = document.querySelector(".loading_screen");
+    loading_screen.classList.toggle("loading_screen_hidden")
+}, 900)
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const homeSection = document.getElementById("home");
+    homeSection.scrollIntoView({});
+});
+
+
+
+
+
+var color;
+
+function SwitchTheme() {
+    var theme = document.body;
+   
+    if(theme.classList.contains("light")){
+        theme.classList.add("dark");
+        theme.classList.remove("light");
+        color = "#f1e9d2b4"
+        ChangeSVGColor();
+    } else if(theme.classList.contains("dark")){
+        theme.classList.add("light");
+        theme.classList.remove("dark");
+        color = "#373534"
+        ChangeSVGColor();
+    };
+
+}
+
+function ChangeSVGColor() {
+    var illustrations = document.getElementsByClassName("illustration");
+
+    for (var i = 0; i < illustrations.length; i++) {
+        var svg = illustrations[i];
+
+        var paths = svg.querySelectorAll("path");
+        var groups = svg.querySelectorAll("g");
+
+        //change all colors in <path></path> tags
+        for (var j = 0; j < paths.length; j++) {
+            paths[j].setAttribute("fill", color);
+            paths[j].setAttribute("stroke", color);
+        }
+
+        //change all colors in <g></g> tags
+        for (var k = 0; k < groups.length; k++) {
+            groups[k].setAttribute("fill", color);
+        }
+        
+    }
+}
+
+
+
+
